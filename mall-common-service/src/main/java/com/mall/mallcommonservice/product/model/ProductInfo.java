f5dca2363e7b3f5d66830c2cc53515db5834af39
wangshupeng <gggcgba@163.com>
2020-11-19 10:58:11 +0800

    1、基础bean要实现序列化

--- mall-common-service/src/main/java/com/mall/mallcommonservice/product/model/ProductInfo.java
+++ mall-common-service/src/main/java/com/mall/mallcommonservice/product/model/ProductInfo.java
@@ -1,11 +1,14 @@
 package com.mall.mallcommonservice.product.model;
 
+import java.io.Serializable;
+
 /**
  * @author gggcgba 【wechat:13031016567】
  * 2020/11/18 0018
  **/
-public class ProductInfo {
+public class ProductInfo implements Serializable {
 
+    private static final long serialVersionUID = 3779275407733692771L;
     /**
      *
      * 产品名称
